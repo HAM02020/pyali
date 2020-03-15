d262d54f0014ecad3e7581f4cfa490b09a45fcee
郝心如 <mofansheng@HAMOdeMacBook-Pro.local>
2020-03-15 17:55:50 +0800

    初次创建项目

---
+++ test01.py
@@ -0,0 +1,13 @@
+with open('pyali/txt.txt') as f:
+    t=f.read()
+print(t)
+print(type(t))
+
+with open('pyali/txt.txt','w') as f:
+    ss="fuck"
+    f.write(ss)
+
+
+with open('pyali/txt.txt') as f:
+    t=f.read()
+print(t)
