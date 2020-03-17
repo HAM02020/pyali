eee8b8b9bf17ed7dc84a25b5d90199f038a37c96
HAM02020 <1106201802@qq.com>
2020-03-18 03:09:03 +0800

    添加parseCategories() 方法 返回商品分类信息列表

--- ali old.py
+++ ali old.py
@@ -27,7 +27,6 @@ headers = {
     'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
 }
 
-
 def gethtml(url):
     try:
         r = requests.get(url, timeout=30, headers=headers)
@@ -36,37 +35,52 @@ def gethtml(url):
         return r.text
     except:
         return "exception"
-
-
 def casheGethtml(url):
     # 文件名
-    file_name = url.replace('.html', '').replace(
-        'https://www.aliexpress.com/', '')+'.txt'
+    file_name=url.replace('https://www.aliexpress.com/','').replace('/','')+'.txt'
     # 初始化缓存文件夹
     dir = os.path.join(os.curdir, 'cashe')
     if not os.path.isdir(dir):
         os.mkdir(dir)
-    # 文件路径
+    #文件路径
     file_dir = os.path.join(dir, file_name)
-
-    # 文件不存在的话创建文件，gethtml并写入缓存文件
+    
+     #文件不存在的话创建文件，gethtml并写入缓存文件
     if not os.path.exists(file_dir):
-        with open(file_dir, 'w+', encoding='utf-8') as f:
+        with open(file_dir,'w+',encoding='utf-8') as f:
             html_str = gethtml(url)
             f.write(html_str)
     # 文件存在 读取缓存
     else:
-        with open(file_dir, 'r+', encoding='utf-8') as f:
-            html_str = f.read()
+        with open(file_dir,'r+',encoding='utf-8') as f:
+            html_str=f.read()
             # 文件存在但是为空
-            if html_str.strip() == '':
-                html_str = gethtml(url)
+            if html_str.strip()=='':
+                html_str=gethtml(url)
                 f.write(html_str)
     return html_str
 
-# 获取分类信息列表
-def parseCategories():
+def tofind(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    uls = soup.find_all('ul', attrs={'class': 'sub-item-cont util-clearfix'})
+    
+    list=[]
+    for ul in uls:
+        aList = ul.find_all('a')
+        for a in aList:
+            dict1 = {}
+            dict1['name'] =a.text
+            dict1['url'] =a['href']
+            list.append(dict1)
+    print(list)
+
+
+if __name__ == "__main__":
+
+    url = 'https://www.aliexpress.com/all-wholesale-products.html'
+    html = casheGethtml(url)
     soup = BeautifulSoup(html, 'lxml')
+
     # 商品分类列表，一个分类对应一个字典{
     # '父分类的名字(如Women's Clothing和Men's Clothing)',
     # '父分类的href(https://www.aliexpress.com/category/100003109/women-clothing.html)',
@@ -76,35 +90,22 @@ def parseCategories():
     # 一共有1-26 26个分类
     for i in range(1, 27):
         category = {}
+        # 通过span标签的id属性 id="anchor + i"找到目标（span的父目录下的a标签为父分类的href+text）
         bigTitleDiv = soup.find("span", {"id": "anchor" + str(i)}).parent
+        # 父分类的名字
         category['parentName'] = bigTitleDiv.a.text
+        #父分类的href
         category['parentHref'] = bigTitleDiv.a['href']
-
-#     取出所有li标签
-#     childrenNameList=list(set(bigTitleDiv.parent.div.div.ul.li.next_siblings))
-#     childrenNameList.remove('\n')
-
-#     取出所有子目录的name和href
-        soupName = BeautifulSoup(
-            str(list(bigTitleDiv.parent.div.div.ul.li.next_siblings)), 'lxml')
-        childrenList = []
-        for c in soupName.find_all('a'):
-            cdict = {}
-            cdict['name'] = c.string
-            cdict['href'] = c['href']
-            childrenList.append(cdict)
-        category['childrenList'] = childrenList
+        # 子分类们
+        childrenNames=' '.join(bigTitleDiv.parent.div.div.ul.li.a.text.split())
+        category['childrenNames'] = childrenNames.split(' ')
+        # 子分类们的href
+#         childrenHrefs=' '.join(bigTitleDiv.parent.div.div.ul.li.a['href'].split())
+#         category['childrenHrefs'] = childrenHrefs.split(' ')
+        category['childrenHrefs'] =bigTitleDiv.parent.div.div.ul.li.a['href'].split(' //')
         categories.append(category)
-        # print(category)
-    return categories
+        print(category['childrenHrefs'])
+        print('名字 = %s href = %s' % (category['childrenNames'],category['childrenHrefs']))
+    
+        
 
-
-if __name__ == "__main__":
-
-    url = 'https://www.aliexpress.com/all-wholesale-products.html'
-    html = casheGethtml(url)
-
-    categories = parseCategories()
-
-    for c in categories:
-        print(c)
